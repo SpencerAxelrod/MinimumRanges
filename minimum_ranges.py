7ee9e98915dcbcbfabece29b32caec154d27e9bc
Spencer Axelrod <spencer.axelrod@gmail.com>
2018-02-05 17:40:55 -0700

    fixed characters issues

--- minimum_ranges.py
+++ minimum_ranges.py
@@ -11,26 +11,26 @@ def find_min_ranges(ranges):
 	min_ranges = []
 	# Repeatedly pop the first range from the list and merge it with another list 
 	# If it cannot be merged, add it to the final min_ranges list 
-	while ranges:  
-		merged = False  
+	while ranges:
+		merged = False
 		current = ranges.pop(0)
 		# Attempt to find another range for current to merge with
-		for range in ranges:	
+		for range in ranges:
 			# Case where current range lower bound is inside another
 			if range[0] <= current[0] <= range[1]:
 				range[1] = max(current[1],range[1])
 				merged = True
-				break		
+				break
 			# Case where current range upper bound is inside another
 			if range[0] <= current[1] <= range[1]:
 				range[0] = min(current[0], range[0])
 				merged = True
-				break		
+				break
 			# Case where current range contains another range
 			if current[0] < range[0] and current[1] > range[1]:
 				range[0], range[1] = current[0], current[1]
 				merged = True
-				break		
+				break
 		# If the current range cannot be merged, it is part of the final min_range list
 		if not merged:
 			min_ranges.append(current)
