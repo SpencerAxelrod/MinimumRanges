a9adb6bf78ed12075b5edd323e19f4f056f454ac
Spencer Axelrod <spencer.axelrod@gmail.com>
2018-02-11 18:44:36 -0700

    - removed unnecessary bool assignment - for loop -> foreach range in ranges - removed explicit <List<Integer>>

--- MinimumRange.java
+++ MinimumRange.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 class MinimumRange {
   public static void main(String args[]) {
-    ArrayList<List<Integer>> r = new ArrayList<List<Integer>>();
+    ArrayList<List<Integer>> r = new ArrayList<>();
     r.add(Arrays.asList(1, 3));
     r.add(Arrays.asList(3, 4));
     r.add(Arrays.asList(5, 6));
@@ -10,24 +10,24 @@ class MinimumRange {
   }
   
   public static ArrayList<List<Integer>> findMinimumRanges(ArrayList<List<Integer>> ranges) {
-    ArrayList<List<Integer>> minRanges = new ArrayList<List<Integer>>();
+    ArrayList<List<Integer>> minRanges = new ArrayList<>();
     while (ranges.size() > 0) {
       boolean merged = false;
       List<Integer> current = ranges.remove(0);
-      for (int i=0; i < ranges.size(); i++) {
-        if (ranges.get(i).get(0) <= current.get(0) && current.get(0) <= ranges.get(i).get(1)) {
-          ranges.get(i).set(1, Math.max(current.get(1), ranges.get(i).get(1)));
+      for (List<Integer> range : ranges) {
+        if (range.get(0) <= current.get(0) && current.get(0) <= range.get(1)) {
+          range.set(1, Math.max(current.get(1), range.get(1)));
           merged = true;
           break;
         }
-        if (ranges.get(i).get(0) <= current.get(1) && current.get(1) <= ranges.get(i).get(1)) {
-          ranges.get(i).set(0, Math.min(current.get(0), ranges.get(i).get(0)));
+        if (range.get(0) <= current.get(1) && current.get(1) <= range.get(1)) {
+          range.set(0, Math.min(current.get(0), range.get(0)));
           merged = true;
           break;
         }
-        if (ranges.get(i).get(0) > current.get(0) && current.get(1) > ranges.get(i).get(1)) {
-          ranges.get(i).set(0, current.get(0));
-          ranges.get(i).set(1, current.get(1));
+        if (range.get(0) > current.get(0) && current.get(1) > range.get(1)) {
+          range.set(0, current.get(0));
+          range.set(1, current.get(1));
           merged = true;
           break;
         }
@@ -35,7 +35,6 @@ class MinimumRange {
       if (!merged) {
         minRanges.add(current);
       }
-      merged = false;
     }
     return minRanges;
   }
